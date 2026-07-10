d765e0e772ee78e0d36d5873f7e77fe7cad7fe32
moqix26 <moqix26@gmail.com>
2026-07-10 22:43:16 +0800

    1

--- Code/test/test.go
+++ Code/test/test.go
@@ -1,41 +1,17 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
+import "fmt"
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "usage: wordcount <file>")
-		os.Exit(1)
-	}
-	f, err := os.Open(os.Args[1])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	defer f.Close()
-
-	freq := make(map[string]int)
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		line := sc.Text()
-		for _, w := range strings.Fields(line) {
-			w = strings.ToLower(strings.Trim(w, ".,!?\"'"))
-			if w == "" {
-				continue
-			}
-			freq[w]++
+func Contains[T comparable](items []T, target T) bool {
+	for _, item := range items {
+		if item == target {
+			return true
 		}
 	}
-	if err := sc.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	for word, count := range freq {
-		fmt.Printf("%s: %d\n", word, count)
-	}
+	return false
+}
+
+func main() {
+	fmt.Println(Contains([]int{1, 2, 3}, 2))
+	fmt.Println(Contains([]string{"go", "java"}, "go"))
 }
